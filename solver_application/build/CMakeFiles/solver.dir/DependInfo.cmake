34840ec631d9e5346e6ae674e1e7ba834fcf4be9
AndrewB203 <bolshakov17419@gmail.com>
2024-05-12 11:44:58 +0300

    add untrackted files

---
+++ solver_application/build/CMakeFiles/solver.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/andrew/AndrewB203/workspacenew/lab03hw/solver_application/equation.cpp" "CMakeFiles/solver.dir/equation.cpp.o" "gcc" "CMakeFiles/solver.dir/equation.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/andrew/AndrewB203/workspacenew/lab03hw/solver_application/build/formatter_ex_lib/CMakeFiles/formatter_ex_lib.dir/DependInfo.cmake"
+  "/home/andrew/AndrewB203/workspacenew/lab03hw/solver_application/build/formatter_ex_lib/formatter_lib/CMakeFiles/formatter_lib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
